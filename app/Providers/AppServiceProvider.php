eeb20b4e3ace41bcc744932a6d616bee35d9e806
Darkbelg <stijn.sagaert@outlook.com>
2021-01-24 14:17:33 +0100

    Change Date Time directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::directive('datetime', function ($expression) {
-            return "<?php echo ($expression)->format('m/d/Y'); ?>";
+            return "<?php echo ($expression)->format('d/m/Y'); ?>";
         });
     }
 }
