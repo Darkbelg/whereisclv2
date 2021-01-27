fe1b6b540d8f9dc077b340a5397b79c27974d7c8
Darkbelg <stijn.sagaert@outlook.com>
2021-01-27 15:01:41 +0100

    Add weekly cron refresh

--- app/Http/Controllers/RefreshController.php
+++ app/Http/Controllers/RefreshController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Tag;
-use App\Models\Video;
 use App\Service\YoutubeApi;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use App\Refresh;
 
 class RefreshController extends Controller
 {
@@ -20,25 +17,7 @@ class RefreshController extends Controller
 
     public function refreshAll()
     {
-        $videos = Video::All();
-        foreach ($videos as $video) {
-            $videoMetaData = $this->youtubeApi->getVideoMetaData($video->id);
-            $videoMetaDataSnippet = $videoMetaData["snippet"];
-            $videoMetaDataStatistics = $videoMetaData["statistics"];
-
-            $video->title = $videoMetaDataSnippet["title"] ?: $video->title;
-            $video->description = $videoMetaDataSnippet["description"] ?: $video->description;
-            $video->comments = $videoMetaDataStatistics["commentCount"] ?: $video->comments;
-            $video->dislikes = $videoMetaDataStatistics["dislikeCount"] ?: $video->dislikes;
-            $video->likes = $videoMetaDataStatistics["likeCount"] ?: $video->likes;
-            $video->views = $videoMetaDataStatistics["viewCount"] ?: $video->views;
-
-            $video->updateTags($videoMetaDataSnippet["tags"])
-                ->updateThumbnails($videoMetaDataSnippet["thumbnails"])
-                ->save();
-        }
-
-        Cache::flush();
+        Refresh::all($this->youtubeApi);
 
         return redirect('/');
     }
---
+++ app/Refresh.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use App\Models\Video;
+use Illuminate\Support\Facades\Cache;
+use App\Service\YoutubeApi;
+
+class Refresh
+{
+    public static function all(YoutubeApi $youtubeApi)
+    {
+        $videos = Video::All();
+        foreach ($videos as $video) {
+            $videoMetaData = $youtubeApi->getVideoMetaData($video->id);
+            $videoMetaDataSnippet = $videoMetaData["snippet"];
+            $videoMetaDataStatistics = $videoMetaData["statistics"];
+
+            $video->title = $videoMetaDataSnippet["title"] ?: $video->title;
+            $video->description = $videoMetaDataSnippet["description"] ?: $video->description;
+            $video->comments = $videoMetaDataStatistics["commentCount"] ?: $video->comments;
+            $video->dislikes = $videoMetaDataStatistics["dislikeCount"] ?: $video->dislikes;
+            $video->likes = $videoMetaDataStatistics["likeCount"] ?: $video->likes;
+            $video->views = $videoMetaDataStatistics["viewCount"] ?: $video->views;
+
+            $video->updateTags($videoMetaDataSnippet["tags"])
+                ->updateThumbnails($videoMetaDataSnippet["thumbnails"])
+                ->save();
+        }
+
+        Cache::flush();
+
+        return true;
+    }
+}
