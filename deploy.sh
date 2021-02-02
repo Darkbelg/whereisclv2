200e6f81435227516d7cbfbd58f3fd87ab56255c
Darkbelg <stijn.sagaert@outlook.com>
2021-02-02 16:07:54 +0100

    Updarte env example Update cache learing command

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,4 @@ git pull
 
 composer install --optimize-autoloader --no-dev
 
-sudo -s /bin/bash -c "php artisan config:cache" -g www-data www-data
-
-sudo -s /bin/bash -c "php artisan route:cache" -g www-data www-data
-
-sudo -s /bin/bash -c "php artisan view:cache" -g www-data www-data
-
-sudo -s /bin/bash -c "php artisan cache:clear" -g www-data www-data
+sudo -s /bin/bash -c "php artisan optimize:clear" -g www-data www-data
