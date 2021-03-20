7103fa732c0a589d061196ea8755776f1d5765f9
Darkbelg <stijn.sagaert@outlook.com>
2021-03-20 14:24:35 +0100

    Add test for console command

--- tests/Feature/RefreshTest.php
+++ tests/Feature/RefreshTest.php
@@ -47,4 +47,28 @@ class RefreshTest extends TestCase
         $this->assertEquals('45', count($videoDatabaseThird->fresh()->tags));
         $this->assertEquals('3000000', $videoDatabaseThird->fresh()->views);
     }
+
+    public function test_console_command(){
+        $videoDatabaseFirst = Video::factory()->create(['id' => "JeGhUESd_1o_1", "views" => "100"]);
+        $videoDatabaseSecond = Video::factory()->create(['id' => "JeGhUESd_1o_2", "views" => "100"]);
+        $videoDatabaseThird = Video::factory()->create(['id' => "JeGhUESd_1o_3", "views" => "100"]);
+
+        $mock = $this->partialMock(YoutubeApi::class, function ($mock) {
+            $mock->shouldReceive('getVideoMetaData')->andReturn(
+                $this->getVideoMetaDataById("JeGhUESd_1o_1"),
+                $this->getVideoMetaDataById("JeGhUESd_1o_2"),
+                $this->getVideoMetaDataById("JeGhUESd_1o_3")
+            );
+        });
+        $this->artisan('youtube:refresh')->assertExitCode(0);
+
+        $this->assertEquals('45', count($videoDatabaseFirst->fresh()->tags));
+        $this->assertEquals('3741389', $videoDatabaseFirst->fresh()->views);
+
+        $this->assertEquals('45', count($videoDatabaseSecond->fresh()->tags));
+        $this->assertEquals('200000', $videoDatabaseSecond->fresh()->views);
+
+        $this->assertEquals('45', count($videoDatabaseThird->fresh()->tags));
+        $this->assertEquals('3000000', $videoDatabaseThird->fresh()->views);
+    }
 }
