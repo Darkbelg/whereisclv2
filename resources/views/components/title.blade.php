eafde7641e85fc56b98b259b18030c45ced600f0
Darkbelg <stijn.sagaert@outlook.com>
2022-02-22 17:47:52 +0100

    Comply with Youtube ToS Violation Report from the audit

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,5 +1,12 @@
-<div>
-    <h1 class="text-6xl p-2 font-serif text-white">#WhereIsCL
-        <span class="leading-none font-sans text-base pl-2">Cherry Picked Videos &amp; Fancams</span>
-    </h1>
+<div class="text-white hover:text-white">
+    @if(!request()->routeIs('home'))
+        <a href="{{ route('home')  }}" class="hover:underline hover:text-white">
+    @endif
+        <h1 class="text-6xl p-2 font-serif ">#WhereIsCL
+            <span class="hidden xl:inline-block leading-none font-sans text-base pl-2">Cherry Picked Videos &amp; Fancams</span>
+        </h1>
+    @if(!request()->routeIs('home'))
+        </a>
+    @endif
+
 </div>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'overview');
+Route::view('/', 'overview')->name('home');
+Route::view('/privacy-policy', 'privacy-policy')->name('privacy-policy');
 
 Route::middleware(['auth'])->group(function (){
     Route::get('video/id/{id}', [VideoController::class, 'getVideoMetaDataById']);
