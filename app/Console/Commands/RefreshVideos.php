200953a0ca414531e1899ccdaa05fb0ab84ce701
Darkbelg <stijn.sagaert@outlook.com>
2022-08-13 21:18:23 +0200

    Add verbose output for commands

--- app/Console/Commands/RefreshVideos.php
+++ app/Console/Commands/RefreshVideos.php
@@ -5,10 +5,13 @@ namespace App\Console\Commands;
 use App\Refresh;
 use App\Service\YoutubeApi;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class RefreshVideos extends Command
 {
     private $youtubeApi;
+    protected $output;
 
     /**
      * The name and signature of the console command.
@@ -29,9 +32,10 @@ class RefreshVideos extends Command
      *
      * @param YoutubeApi $youtubeApi
      */
-    public function __construct(YoutubeApi $youtubeApi)
+    public function __construct(YoutubeApi $youtubeApi, OutputInterface $output)
     {
         $this->youtubeApi = $youtubeApi;
+        $this->output = $output;
         parent::__construct();
     }
 
@@ -45,6 +49,11 @@ class RefreshVideos extends Command
         try {
             Refresh::all($this->youtubeApi);
         } catch (\Exception $e) {
+            $this->output->writeln(
+                $e->getMessage(),
+                OutputInterface::VERBOSITY_DEBUG
+            );
+            Log::error($e);
             $this->error('Something went wrong!');
             return 1;
         }
